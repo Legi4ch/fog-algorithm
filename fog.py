f4969863b5d25df8a77e2afb3be41b89eae72191
Legi4ch <sorokin.oleg@gmail.com>
2023-12-06 14:05:48 +0300

    Algorithm fixed to improve entropy, code refactoring

--- fog.py
+++ fog.py
@@ -15,20 +15,17 @@ class Fog:
 
     def __init__(self, _pin: str, _seed_len: int):
         self.seed_len = _seed_len
-        self.pin = _pin
+        self._pin = self._glue_pin(_pin)
         self._set_primary_key()
         self._set_xor_key()
         self._set_shifts()
 
-    def _glue_pin(self) -> str:
-        """ формируем предварительный ключ длиной seed_len из символов pin """
-        if len(self.pin) >= self.seed_len:
-            return self.pin[0:self.seed_len]
-        else:
-            key = ''
-            while len(key) < self.seed_len:
-                key = key + self.pin
-            return key[0:self.seed_len]
+    def _glue_pin(self, _pin: str):
+        split_pin = _pin.split()
+        result_list = []
+        while len(result_list) < self.seed_len:
+            result_list.extend(split_pin)
+        return result_list[:self.seed_len]
 
     def _set_primary_key(self):
         """
@@ -36,10 +33,10 @@ class Fog:
         """
         result_key = []
         key_sum = 0
-        key = self._glue_pin()
+        key = self._pin
 
-        for i in range(0, len(key)):
-            el = int(key[i]) + ((i+1) * 10) if i % 2 > 0 else int(key[i]) ** 2
+        for idx in range(0, len(key)):
+            el = int(key[idx]) + (idx+1)*10 if idx % 2 > 0 else int(key[idx]) ** 2
             result_key.append(el)
             key_sum = key_sum + el
 
@@ -51,8 +48,8 @@ class Fog:
         """
         result_key = []
         key_sum = 0
-        for i in range(0, len(self._primary_key)):
-            xor = self._primary_key[i] ^ (i+1)
+        for idx in range(0, len(self._primary_key)):
+            xor = self._primary_key[idx] ^ (idx+1)
             result_key.append(xor)
             key_sum = key_sum + xor
 
@@ -64,13 +61,15 @@ class Fog:
             Формируем итоговый сдвиг каждого слова
         """
         result_key = []
-        for i in range(0, len(self._xor_key)):
-            if i % 2 > 0:
-                el = self._xor_key[i] + self._primary_key[i] - self._xor_control_sum
-            else:
-                el = self._xor_key[i] + self._primary_key[i] + self._xor_control_sum
+        for idx in range(0, len(self._xor_key)):
+            i = self._xor_key[idx] if self._xor_key[idx] < 1000 else int(str(self._xor_key[idx])[:3]) + (idx+1)
+            el = i - self._xor_control_sum if idx % 2 > 0 else i + self._xor_control_sum
+            el = -el if el < 0 else el
             result_key.append(el)
         self._shifts = result_key
+
+    def get_pin(self) -> list:
+        return self._pin
 
     def get_primary_key(self) -> list:
         return self._primary_key
@@ -117,15 +116,16 @@ class Fog:
 
 
 if __name__ == "__main__":
-    pin = '12345'
+    pin = '1 2 3 4 5'
     phrase = '''1 2 3 4 5 6 7 8 9 10 11 12'''
     seed_len = len(phrase.split())
-    fog = Fog(pin, seed_len)
+    fog = Fog(pin, 12)
 
     # замешивание тумана
     result = fog.get_fog(phrase)
     print("Fog: ", fog.list_to_str(result))
 
+    print("Pin: ", fog.get_pin())
     print("Key: ", fog.get_primary_key())
     print("XOR: ", fog.get_xor_key())
     print("Shifts: ", fog.get_shifts())
